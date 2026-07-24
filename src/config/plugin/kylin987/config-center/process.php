6fead99b389a0dd609f18eb1f02060aa39a89e66
wind <kylin87@163.com>
2026-07-24 11:49:40 +0800

    refactor: rely on webman plugin enable switch

--- src/config/plugin/kylin987/config-center/process.php
+++ src/config/plugin/kylin987/config-center/process.php
@@ -1,9 +1,5 @@
 <?php
 
-if (!config('plugin.kylin987.config-center.app.enable', true)) {
-    return [];
-}
-
 return [
     'config-center' => [
         'handler' => \Kylin987\WebmanConfigCenter\Process\ConfigCenterProcess::class,
